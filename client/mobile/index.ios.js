b6f3fc2b5f1fa292ff45185f5535a36a455a9105
Guy Thomas <guythomas721@gmail.com>
2016-10-23 20:49:05 -0700

    Add Map With Basic Header

--- client/mobile/index.ios.js
+++ client/mobile/index.ios.js
@@ -3,51 +3,58 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
   View
 } from 'react-native';
+import Header from './src/components/Header';
+import SafeMap from './src/components/SafeMap';
 
 export default class mobile extends Component {
-  render() {
+
+state = {
+	region: {
+	latitude: 0,
+	longitude: 0,
+	latitudeDelta: 0.0922,
+	longitudeDelta: 0.0421,
+	}
+}
+
+
+onRegionChange(region) {
+  this.setState({ region });
+}
+
+componentWillMount() {
+	this.setState({
+		region: {
+			latitude: 37.78825,
+			longitude: -122.4324,
+			latitudeDelta: 0.0922,
+			longitudeDelta: 0.0421,
+		}
+	});
+}
+
+ render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+    <View style={styles.mapContainer}>
+		<Header headerText={'Safe Hippo'} />
+		<SafeMap 
+			onRegionChange={this.onRegionChange.bind(this)}
+			region={this.state.region}
+		/>
+	</View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+const styles = {
+	mapContainer: {
+		flex: 1
+	}
+};
 
 AppRegistry.registerComponent('mobile', () => mobile);
