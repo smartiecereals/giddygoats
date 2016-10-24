e38bd17586acfec1770f3adc3a818370477a768b
bianca <bianca.subion@gmail.com>
2016-10-24 14:30:36 -0700

    set up postgres

--- server/database/config.js
+++ server/database/config.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://kgqqsuuipxvzaw:wLwszHMSXLu8BW_leThaZbHZyV@ec2-54-163-238-222.compute-1.amazonaws.com:5432/d593peshjelr10:5678')
-
+var db = new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+});
 
 module.exports = db;
