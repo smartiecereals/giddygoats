8290d780484c20aefed53b2194e07b4ce735f575
bianca <bianca.subion@gmail.com>
2016-10-24 14:17:02 -0700

    test postgres

--- server/database/config.js
+++ server/database/config.js
@@ -1,6 +1,12 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://127.0.0.1:5432/dangerDB');
+var db = new Sequelize(process.env.HEROKU_POSTGRESQL_YELLOW_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  port: match[4],
+  host: match[3],
+  logging: true
+});
 
 
 module.exports = db;
