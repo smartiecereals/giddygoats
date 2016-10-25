d52cbc8bfff42cc5917d4baf402fab8a8b8c9231
Sam Henderson <hendersonsam456@gmail.com>
2016-10-25 16:21:35 -0700

    implemented acqaure side bar basic formating

--- server/express-server.js
+++ server/express-server.js
@@ -23,7 +23,6 @@ app.get('/shortestRoute', function(req, res) {
   var destLon = req.query.destLon;
   var waypoints = [];
 
-
   var url = 'https://maps.googleapis.com/maps/api/directions/json?'
 
   var queryUrl = 
@@ -69,11 +68,7 @@ app.get('/testDanger', function(req, res) {
     res.send(200, newArr);
 
   });
-
 });
-
-
-
 
 app.listen(app.get('port'), function() {
   console.log('Listening in on http://' + ip + ':' + port);
