d9c44d192b882c171973d2366dccdd5e6ac55d95
Sam Henderson <hendersonsam456@gmail.com>
2016-10-26 17:29:18 -0700

    onclick "address" displays a "origin" field that submits successfully to the view controller

--- client/web/app/viewer.js
+++ client/web/app/viewer.js
@@ -2,42 +2,53 @@ angular.module('app.controllers', [])
 
 .controller('ViewController', function($scope) {
 
+  // mapLoaded and 'flipMapLoaded' are used to tell if the map has loaded.
+  // We use them to determine when to display the spinner
+
   $scope.mapLoaded = true;
+  $scope.showOriginField = false;
 
   $scope.flipMapLoaded = function() {
     $scope.mapLoaded = !$scope.mapLoaded;
     $scope.$apply();
   };
 
+  $scope.editHandler = function() {
+    console.log('$scope.showOriginField: ', $scope.showOriginField);
+    $scope.showOriginField = !$scope.showOriginField;
+    console.log('$scope.showOriginField: ', $scope.showOriginField);
+  };
+
+  // setPos converts user's (lon,lat) to street address and append's 
+  // address to the $scope object
 
   $scope.setPos = function(currentPosition) {
-    
     var url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + currentPosition.lat + ',' + currentPosition.lng + '&key=AIzaSyBgXiNUqN5OlBHE7hAVxV9phqHQrfKldXw';
     fetch(url)
     .then(function(addressData) {
       return addressData.json()
     })
     .then(function(addressDataJSON) {
-      console.log('addressDataJSON :', addressDataJSON);
       var longAddress = addressDataJSON.results[0].formatted_address;
-      var arrSplit = "25 Mason St, San Francisco, CA 94102, USA".split(',');
+      var arrSplit = longAddress.split(',');
       shortAddress = arrSplit[0] + ',' + arrSplit[1];
       currentPosition.address = shortAddress;
       $scope.pos = currentPosition;
       $scope.$apply();
-      console.log('$scope.pos :', $scope.pos);
     })
-
   }
 
-  $scope.submitHandler = function(destination, mobile) {
+  // submitHandler takes the form values and does [INSERT PURPOSE]
+
+  $scope.submitHandler = function(destination, mobile, origin) {
     console.log('inside the Submit Handler');
     console.log('destination: ', destination);
     console.log('mobile: ', mobile);
+    console.log('origin: ', origin);
 
-    navigator.geolocation.getCurrentPosition(function(location) {
-      console.log('location in the "then" callback: ', location);
-    })
+    // navigator.geolocation.getCurrentPosition(function(location) {
+    //   console.log('location in the "then" callback: ', location);
+    // })
   }
   
 });
