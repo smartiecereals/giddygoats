9c49a4e30ed8675c4cadc948b5509e600e8e71cb
spiritphyz <tonyle@ocf.berkeley.edu>
2016-11-01 18:43:22 -0700

    Add notes about deployment on Ubuntu

---
+++ server/server_notes/sample query from google maps api.js
@@ -0,0 +1,102 @@
+{
+  "results": [
+    {
+      "address_components": [
+        {
+          "long_name": "944",
+          "short_name": "944",
+          "types": [
+            "street_number"
+          ]
+        },
+        {
+          "long_name": "Market Street",
+          "short_name": "Market St",
+          "types": [
+            "route"
+          ]
+        },
+        {
+          "long_name": "Tenderloin",
+          "short_name": "Tenderloin",
+          "types": [
+            "neighborhood",
+            "political"
+          ]
+        },
+        {
+          "long_name": "San Francisco",
+          "short_name": "SF",
+          "types": [
+            "locality",
+            "political"
+          ]
+        },
+        {
+          "long_name": "San Francisco County",
+          "short_name": "San Francisco County",
+          "types": [
+            "administrative_area_level_2",
+            "political"
+          ]
+        },
+        {
+          "long_name": "California",
+          "short_name": "CA",
+          "types": [
+            "administrative_area_level_1",
+            "political"
+          ]
+        },
+        {
+          "long_name": "United States",
+          "short_name": "US",
+          "types": [
+            "country",
+            "political"
+          ]
+        },
+        {
+          "long_name": "94102",
+          "short_name": "94102",
+          "types": [
+            "postal_code"
+          ]
+        }
+      ],
+      "formatted_address": "944 Market St, San Francisco, CA 94102, USA",
+      "geometry": {
+        "bounds": {
+          "northeast": {
+            "lat": 37.7838656,
+            "lng": -122.4088346
+          },
+          "southwest": {
+            "lat": 37.7835276,
+            "lng": -122.4090982
+          }
+        },
+        "location": {
+          "lat": 37.7836966,
+          "lng": -122.4089664
+        },
+        "location_type": "ROOFTOP",
+        "viewport": {
+          "northeast": {
+            "lat": 37.7850455802915,
+            "lng": -122.4076174197085
+          },
+          "southwest": {
+            "lat": 37.7823476197085,
+            "lng": -122.4103153802915
+          }
+        }
+      },
+      "place_id": "ChIJa2U7loWAhYARXXIPjJLMNcM",
+      "types": [
+        "premise"
+      ]
+    }
+  ],
+  "status": "OK"
+}
