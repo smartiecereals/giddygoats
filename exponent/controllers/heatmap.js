7e6cd3e3147f2451cb5d9f3876c1cdd5cbe47b06
Mach13 <amad_khan1@hotmail.com>
2016-11-02 17:16:49 -0700

    Work on heatmap

--- exponent/controllers/heatmap.js
+++ exponent/controllers/heatmap.js
@@ -1,10 +1,18 @@
-import ReactHeatmap from 'react-heatmap';
 import axios from 'axios';
 
-// let data = axios.get()
+let data = [];
 
-render () {
+axios.get('http://138.68.62.73:3000/testDanger?long=-122.40898400000003&lat=37.7865518&radius=.01')
+      .then(function(res) {
+        res.forEach(function(dataPoint) {
+          var dataObj = {x: dataPoint[0], y: dataPoint[1]}
+          data.push(dataObj)
+        })
+      })
+
+let HeatMap = () => {
   return (
     <ReactHeatmap data={data} />
   )
 }
+export default HeatMap
