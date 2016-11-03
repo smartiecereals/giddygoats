ceadf57fae0526f5ff0c2e81bbfb73e7fafe9877
meijiao <meijiao@gmail.com>
2016-11-02 19:09:57 -0700

    Add text input for origin

--- exponent/index.js
+++ exponent/index.js
@@ -15,6 +15,7 @@ class App extends React.Component {
   constructor () {
     super ();
     this.state = {
+      
     };
 
   this.handleUserDestinationInput = this.handleUserDestinationInput.bind(this);
--- exponent/userInput.js
+++ exponent/userInput.js
@@ -5,8 +5,14 @@ import {View, Text, TextInput} from 'react-native';
 
 let UserInput = (props) => {
 
+  var origin = props.origin;
+
     return (
         <View style={styles.inputContainer}>
+          <TextInput style={styles.textBox}
+            placeholder={origin}
+            onChangeText={(text) => props.handleUserOriginInput(text)}
+          />
           <TextInput style={styles.textBox}
             placeholder="Destination"
             onChangeText={(text) => props.handleUserDestinationInput(text)}
