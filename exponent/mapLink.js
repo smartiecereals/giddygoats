1f992ac21e0b2c19d22bb1787977ec5de1df1e7d
Mach13 <amad_khan1@hotmail.com>
2016-11-04 22:42:52 -0700

    Fix merge conflicts

--- exponent/mapLink.js
+++ exponent/mapLink.js
@@ -6,35 +6,36 @@ import {View, Text, TextInput, Image, TouchableOpacity, Linking} from 'react-nat
 
 let MapLink = (props) => {
 
-    return (
-      <Image
-        source={require('./assets/images/gradient90_1024_320.png')}
-        style={styles.gradient}
+  return (
+    <Image
+      source={require('./assets/images/gradient90_1024_320.png')}
+      style={styles.gradient}
+    >
+      <Image 
+        source={require('./assets/images/app-icon-safe-hippo_96.png')} 
+        style={styles.logo}
+      />
+      <TouchableOpacity
+      key='googleMapsUrl'
+      style={styles.googleMapsTouch}
+      onPress={()=>Linking.openURL(props.googleMapsUrl)}
       >
-        <Image 
-          source={require('./assets/images/app-icon-safe-hippo_96.png')} 
-          style={styles.logo}
-        />
-        <TouchableOpacity
-        key='googleMapsUrl'
-        style={styles.googleMapsTouch}
-        onPress={()=>Linking.openURL(props.googleMapsUrl)}
+        <Animatable.Text 
+          animation="fadeInUp" 
+          style={{color:'#FFF'}, styles.googleMapsLink} 
+          duration={1500}
         >
-          <Animatable.Text 
-            animation="fadeInUp" 
-            style={{color:'#FFF'}, styles.googleMapsLink} 
-            duration={1500}
-          >
-            Open in Google Maps
-          </Animatable.Text>
-        </TouchableOpacity>
+          Open in Google Maps
+        </Animatable.Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={props.toggleCrime}>
         <Image 
           source={require('./assets/images/toggle-crime_96.png')} 
           style={styles.toggleCrime}
         />
-      </Image>
-    );
-  
+      </TouchableOpacity>  
+   </Image>
+  );
 }
 
 // let MapLink = () => {
